bd4a7b91aa7ae3556ffbf49ff491d1fa4dea0b98
lukas <lukas-sinzing@gmx.de>
2023-08-26 13:16:30 +0200

    Add project files.

---
+++ xkcd-scraper/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace xkcd_scraper;
+
+internal class Program
+{
+    const int ItemCount = 2812;
+
+    static int Main(string[] args)
+    {
+        return MainAsync(args).Result;
+    }
+    static async Task<int> MainAsync(string[] args)
+    {
+        // first get all items in the list
+        var urls = new string[ItemCount].Select((x, i) => $"https://xkcd.com/{i + 1}/info.0.json").ToArray();
+
+        var func = async (string x) =>
+        {
+            try
+            {
+                var decoded = JsonConvert.DeserializeObject<Dictionary<string, object>>(x);
+                
+                Console.WriteLine($"Decoded item {(long)decoded["num"]}");
+
+                return decoded;
+            }
+            catch
+            {
+                return null;
+            }
+        };
+
+        var results = await WebHelpers.GetAndProcess(urls, func);
+
+        results = results.Where(x => x != null).ToArray();
+
+        urls = results.Select(x => (string)x["img"]).ToArray();
+
+        var func2 = async (byte[] data, int index, Dictionary<string, object> arg) =>
+        {
+            await File.WriteAllBytesAsync(MakePathSafe($"xkcd/{index} - {((string)arg["title"]).Replace("/", " ")}.png"), data);
+            Console.WriteLine($"Wrote '{index} - {(string)arg["title"]}'.png");
+            return true;
+        };
+
+        Directory.CreateDirectory("xkcd");
+
+        await WebHelpers.GetAndProcessBinary<bool, Dictionary<string, object>>(urls, func2, results);
+
+        return 0;
+    }
+
+    static string MakePathSafe(string path)
+    {
+        // Remove invalid characters
+        string safePath = string.Join("_", path.Split(Path.GetInvalidFileNameChars()));
+
+        return safePath;
+    }
+}
---
+++ xkcd-scraper/WebHelpers.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+
+using System.Collections;
+using System.ComponentModel;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace xkcd_scraper;
+
+internal class WebHelpers
+{
+    public static int RetryCount = 10;
+    public static int RetryDelay = 1000;
+    private static HttpClient _client = new HttpClient();
+
+    private static async Task<T> ExecuteWithRetry<T>(Func<Task<T>> action, int retryCount, int  retryDelay)
+    {
+        var tries = 0;
+
+        while (tries < retryCount  - 1)
+        {
+            try
+            {
+                return await action();
+            }
+            catch
+            {
+                tries++;
+                await Task.Delay(retryDelay);
+            }
+        }
+
+        return await action();
+    }
+
+    public static async Task<string> SimpleGet(string address)
+    {
+        var result = await ExecuteWithRetry(() => _client.GetAsync(address), RetryCount, RetryDelay);
+
+        // XKCD nr. 404 doesn't exist...
+        // we'll manage the returned error later on
+        if (!address.Contains("404"))
+            result.EnsureSuccessStatusCode();
+
+        var str = await result.Content.ReadAsStringAsync();
+
+        return str;
+    }
+
+    public  static IEnumerable<Task<byte[]>> SimpleGetsTasksBinary(string[] addresses)
+    {
+        return addresses.Select(SimpleGetBinary);
+    }
+
+    public static IEnumerable<Task<string>> SimpleGetsTasks(string[] addresses)
+    {
+        return addresses.Select(SimpleGet);
+    }
+
+    public static async Task<string[]> SimpleGets(string[] addresses)
+    {
+        return await Task.WhenAll(SimpleGetsTasks(addresses));
+    }
+
+    public static async Task<byte[]> SimpleGetBinary(string address)
+    {
+
+        var result = await ExecuteWithRetry(() => _client.GetAsync(address), RetryCount, RetryDelay);
+
+        return await result.Content.ReadAsByteArrayAsync();
+    }
+
+    public static async Task<byte[][]> SimpleGetsBinary(string[] addresses)
+    {
+        return await Task.WhenAll(addresses.Select(SimpleGetBinary));
+    }
+
+    public static async Task<T[]> GetAndProcess<T>(string[] addresses, Func<string, Task<T>> func)
+    {
+        var tasks = SimpleGetsTasks(addresses).ToArray();
+        var results = new T[addresses.Length];
+
+        while (tasks.Any(x => x != null))
+        {
+            var t = await Task.WhenAny(tasks.Where(x => x != null).ToArray());
+
+            var taskIndex = IndexOf(tasks, t);
+            tasks[taskIndex] = null;
+
+            results[taskIndex] = await func(t.Result);
+        }
+
+        return results;
+    }
+
+    public static async Task<T[]> GetAndProcessBinary<T, TArg>(string[] addresses, Func<byte[], int, TArg, Task<T>> func, TArg[] funcArgs)
+    {
+        var tasks = SimpleGetsTasksBinary(addresses).ToArray();
+        var processingTasks = new Task<T>[addresses.Length];
+
+        while (tasks.Any(x => x != null))
+        {
+            var t = await Task.WhenAny(tasks.Where(x => x != null).ToArray());
+
+            var taskIndex = IndexOf(tasks, t);
+            tasks[taskIndex] = null;
+
+            processingTasks[taskIndex] = func(t.Result, taskIndex, funcArgs[taskIndex]);
+        }
+
+        Task.WaitAll(processingTasks);
+
+        return processingTasks.Select(x => x.Result).ToArray();
+    }
+
+    private static int IndexOf<T>(T[] items, T item)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] != null && items[i].Equals(item))
+                return i;
+        }
+
+        throw new Exception("Item not in list!");
+    }
+}
